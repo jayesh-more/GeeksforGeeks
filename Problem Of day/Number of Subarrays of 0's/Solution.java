685ff9951b591d338ece01a12056a8fc400807e0
Jayesh More <65273915+jayesh-more@users.noreply.github.com>
2023-03-17 09:01:59 +0530

    Create Solution.java

---
+++ Problem Of day/Number of Subarrays of 0's/Solution.java
@@ -0,0 +1,26 @@
+class Solution{
+	long no_of_subarrays(int N, int [] arr) {
+		
+		long ans=0;
+		long cnt=0;
+		
+		for(var in:arr)
+		{
+		    if(in==0)
+		    {
+		        cnt++;
+		    }
+		    else
+		    {
+		        ans+=((long)(cnt*(cnt+1))/2);
+		        cnt=0;
+		    }
+		}
+		if(cnt!=0)
+		{
+		    ans+=((long)(cnt*(cnt+1))/2);
+		}
+		
+		    return ans;
+	}
+}
